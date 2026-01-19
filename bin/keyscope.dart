31d90c2ae35ecc54ab7d0cbf130f1cf8064643fc
Taeha Kim <taeha.kim@infradise.com>
2026-01-20 07:03:53 +0900

    feat: add set/get/ping etc to CLI.

--- bin/keyscope.dart
+++ bin/keyscope.dart
@@ -14,136 +14,238 @@
  * limitations under the License.
  */
 
+import 'dart:async';
 import 'dart:io';
 import 'package:args/args.dart';
-// import 'package:keyscope/keyscope.dart'; // TODO: keyscope_cli.dart
-// import 'package:valkey_client/valkey_client.dart';
 import 'package:keyscope/src/core/keyscope_client.dart';
+import 'package:valkey_client/valkey_client.dart';
+
+ValkeyLogger logger = ValkeyLogger('Keyscope CLI');
 
 void main(List<String> arguments) async {
-  // e.g., keyscope --ping
+  final setCommand = ArgParser()
+    ..addOption('key', abbr: 'k', mandatory: true, help: '--key version')
+    ..addOption('value', abbr: 'v', mandatory: true, help: '--value 9.0.0');
+
+  final getCommand = ArgParser()
+    ..addOption('key',
+        abbr: 'k', mandatory: true, help: '--key version \n=> return 9.0.0');
+
+  // Check connectivity (PING/PONG)
+  final pingCommand = ArgParser();
+  // ..addFlag('all', abbr: 'a', help: 'all nodes', negatable: false);
+
   final parser = ArgParser()
     ..addOption('host', abbr: 'h', defaultsTo: 'localhost', help: 'Target host')
     ..addOption('port', abbr: 'p', defaultsTo: '6379', help: 'Target port')
+    ..addOption('username', abbr: 'u', help: 'Username')
     ..addOption('password', abbr: 'a', help: 'Password')
+    ..addOption('db', help: 'Database (defaults to 0)')
+    ..addFlag('ssl', help: 'SSL/TLS/mTLS', negatable: false)
+    ..addFlag('silent', help: '(Silent mode) Hide all logs', negatable: false)
     ..addOption('match',
         abbr: 'm', defaultsTo: '*', help: 'Key pattern to match (for --scan)')
-    ..addFlag('ping', help: 'Check connectivity (PING/PONG)', negatable: false)
     ..addFlag('scan',
         help: 'Scan keys (Cursor-based iteration)', negatable: false)
+    ..addCommand('set', setCommand)
+    ..addCommand('get', getCommand)
+    ..addCommand('ping', pingCommand)
+    ..addMultiOption('set', help: '--set <key> <value>')
+    ..addMultiOption('get', help: '--get <key>')
     ..addFlag('help',
         abbr: '?', help: 'Show usage information', negatable: false);
 
   try {
     final results = parser.parse(arguments);
 
+    logger.setEnableValkeyLog(!(results['silent'] as bool));
+
     if (results['help'] as bool) {
-      print('Keyscope CLI - Diagnostic Tool\n');
-      print('Usage: keyscope [options]');
-      print(parser.usage);
+      showUsages(parser);
       exit(0);
     }
 
     final host = results['host'] as String;
     final port = int.parse(results['port'] as String);
+    final username = results['username'] as String?;
     final password = results['password'] as String?;
+    final db = results['db'] as int? ?? 0;
+    final ssl = results['ssl'] as bool? ?? false;
     final match = results['match'] as String;
 
     // 1. Instantiate the repository directly (No Riverpod needed for CLI)
-    // final repo = BasicConnectionRepository();
-    // TODO: Use client instead of repo for now.
+    // -- final repo = BasicConnectionRepository();
+    // -- await repo.connect(host: host, port: port, password: password);
     // Use the Pure Dart Client
     final client = KeyscopeClient();
 
+    final settings = ValkeyConnectionSettings(
+      host: host,
+      port: port,
+      username: username,
+      password: password,
+      useSsl: ssl,
+      database: db,
+    );
+
+    final valkeyClient = ValkeyClient.fromSettings(settings);
+
+    // 2. Connect
+    await connect(valkeyClient);
+
     try {
-      // 2. Connect
-      // print('🔌 Connecting to $host:$port...');
-      // await repo.connect(host: host, port: port, password: password);
-      // print('✅ Connected.');
-
       // 3. Handle Commands
-      if (results['ping'] as bool) {
-        // [PING Test]
-        // await _runPingTest(host, port);
-        // Since we are connected, basic connectivity is already proven.
-        // We can optionally check info or just print OK.
-        // print('🏓 PING Test: OK (Connection established)');
+      switch (results.command?.name) {
+        case 'ping':
+          await ping(valkeyClient);
+          await close(valkeyClient);
+          break;
+        case 'set':
+          final key = results.command?['key'] as String;
+          final value = results.command?['value'] as String;
+          await set(valkeyClient, key, value);
+          break;
+        case 'get':
+          final key = results.command?['key'] as String;
+          await get(valkeyClient, key);
+          break;
+        case 'scan':
+          break;
+        default:
+          // Handle Options
+          //
+          // if (results['ping'] as bool) {}
+          //
+          if (results.wasParsed('set')) {
+            final values = results['set'] as List<String>;
+            if (values.length >= 2) {
+              final key = values[0];
+              final value = values[1];
+              await set(valkeyClient, key, value);
+            } else {
+              logger.info(parser.usage);
+            }
+          } else if (results.wasParsed('get')) {
+            final values = results['get'] as List<String>;
+            if (values.isNotEmpty) {
+              final key = values[0];
+              await get(valkeyClient, key);
+            } else {
+              logger.info(parser.usage);
+            }
+          } else if (results.wasParsed('scan')) {
+            // TODO: support both command and option
+            // TODO: change to ValkeyClient
+            logger.info('🔌 Connecting to $host:$port...');
+            await client.connect(host: host, port: port, password: password);
 
-        print('🔌 Connecting to $host:$port...');
-        await client.connect(host: host, port: port, password: password);
-        print('✅ PING: OK (Connection Established)');
-      } else if (results['scan'] as bool) {
-        print('🔌 Connecting to $host:$port...');
-        await client.connect(host: host, port: port, password: password);
+            logger.info('🔍 Scanning keys (MATCH: "$match", COUNT: 20)...');
 
-        // [SCAN Test]
-        // print('🔍 Scanning keys (MATCH: "$match")...');
-        print('🔍 Scanning keys (MATCH: "$match", COUNT: 20)...');
+            final result =
+                await client.scanKeys(cursor: '0', match: match, count: 20);
 
-        final result =
-            await client.scanKeys(cursor: '0', match: match, count: 20);
+            // logger.info('----------------------------------------');
+            // logger.info('Next Cursor : ${result.cursor}');
+            // logger.info('Found Keys  : ${result.keys.length}');
+            // logger.info('----------------------------------------');
+            logger.info('Found ${result.keys.length} keys. '
+                'Next cursor: ${result.cursor}');
 
-        // print('----------------------------------------');
-        // print('Next Cursor : ${result.cursor}');
-        // print('Found Keys  : ${result.keys.length}');
-        // print('----------------------------------------');
-        print(
-            'Found ${result.keys.length} keys. Next cursor: ${result.cursor}');
+            if (result.keys.isEmpty) {
+              logger.info('(No keys found)');
+            } else {
+              for (var key in result.keys) {
+                logger.info('- $key');
+              }
+            }
 
-        if (result.keys.isEmpty) {
-          print('(No keys found)');
-        } else {
-          for (var key in result.keys) {
-            print('- $key');
+            if (result.cursor == '0') {
+              logger.info('----------------------------------------');
+              logger.info('✅ Full iteration completed (Cursor returned to 0).');
+            } else {
+              logger.info('----------------------------------------');
+              logger.info('👉 More keys available. '
+                  'Use cursor "${result.cursor}" to continue.');
+            }
+          } else {
+            showUsages(parser);
           }
-        }
-
-        if (result.cursor == '0') {
-          print('----------------------------------------');
-          print('✅ Full iteration completed (Cursor returned to 0).');
-        } else {
-          print('----------------------------------------');
-          print('👉 More keys available. Use cursor "${result.cursor}" to '
-              'continue.');
-        }
-      } else {
-        // print('No command specified. Use --help for usage.');
-        // print('⚠️ No command. Use --ping or --scan');
-        print(
-            '⚠️ No command specified. Use --ping or --scan. Otherwise, --help '
-            'for usage.');
-        print(parser.usage);
       }
+    } on FormatException catch (e) {
+      logger.info(e.message);
+      showUsages(parser);
     } catch (e) {
-      print('❌ Error: $e');
+      logger.error('❌ Error: $e');
       exit(1);
     } finally {
       // Cleanup
       await client.disconnect();
+      await close(valkeyClient);
     }
   } catch (e) {
-    // print('Error: $e');
-    // print('❌ Args Error: $e');
-    print('❌ Invalid arguments: $e');
-    print(parser.usage);
+    logger.error('❌ Invalid arguments: $e'); // Args Error
     exit(1);
   }
 }
 
-// Future<void> _runPingTest(String host, int port) async {
-//   print('Running PING test on $host:$port...');
+Future<String> ping(ValkeyClient client) async {
+  logger.info('🏓 PING');
+  try {
+    final response = await client.ping();
+    logger.info('RESPONSE: '
+        '${response.toString() == 'PONG' ? 'PONG' : 'NOT PONG ($response)'}');
+    // logger.info('✅ Connection Status: OK');
+    return response;
+  } catch (e) {
+    logger.error('❌ Connection Failed: $e');
+    return ''; // OR exit(1);
+  }
+}
 
-//   // Here we use the Repository logic defined in lib/
-//   // Since we are in CLI (no Riverpod), we instantiate the repository directly.
-//   final repo = BasicConnectionRepository();
+Future<void> connect(ValkeyClient client) async {
+  final config = client.currentConnectionConfig;
+  logger.info('Target host: ${config?.host}');
+  logger.info('Target port: ${config?.port}');
 
-//   try {
-//     // In the future, this will return real connection status
-//     await repo.connect(host: host, port: port);
-//     print('✅ Connection Status: OK');
-//     // TODO: Implement actual PING command via valkey_client
-//   } catch (e) {
-//     print('❌ Connection Failed: $e');
-//     exit(1);
+  logger.info('🔌 Connecting to ${config?.host}:${config?.port}...');
+  await client.connect();
+}
+
+// Future<void> showCurrentConnectedHostAndPort(ValkeyClient client) async {
+//   final config = client.currentConnectionConfig;
+//   logger.info(client.isConnected);
+//   if (client.isConnected && config != null) {
+//     logger.info('Connected host: ${config.host}');
+//     logger.info('Connected port: ${config.port}');
+//   } else {
+//     logger.info('No connection');
 //   }
 // }
+
+Future<void> close(ValkeyClient client) async {
+  await client.close();
+}
+
+Future<String?> get(ValkeyClient client, String key) async {
+  final value = await client.get(key); // unawaited(client.get(key));
+  logger.info('GET: key: $key, value: $value');
+  return value;
+}
+
+Future<void> set(ValkeyClient client, String key, String value) async {
+  logger.info('SET: key: $key, value: $value');
+  await client.set(key, value); // unawaited(client.set(key, value));
+}
+
+// ⚠️ When no command specified:
+void showUsages(ArgParser parser) {
+  print('Keyscope CLI - Diagnostic and CI Tool\n');
+  print('Usage: keyscope <command> [options]\n');
+  print('Commands:');
+  parser.commands.forEach((name, cmd) {
+    print('\n$name:');
+    print(cmd.usage);
+  });
+  print('\nOptions:');
+  print(parser.usage);
+}
